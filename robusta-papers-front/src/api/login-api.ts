abddbfb55d94c9eedfdb022fa98a2f7a1d387078
Nicolas Zozol <nzozol@gmail.com>
2020-03-10 09:45:09 +0100

    Register payload reformat

--- robusta-papers-front/src/api/login-api.ts
+++ robusta-papers-front/src/api/login-api.ts
@@ -3,13 +3,18 @@ import {apiRoot} from './api';
 import {User} from "./user.model";
 
 const loginApi = apiRoot + 'login';
-const registerApi = apiRoot + 'login';
+const registerApi = apiRoot + 'register';
 
-export function login(username: string, password: string): Promise<User> {
-    return axios.post(loginApi, {username, password})
+export function login(email: string, password: string): Promise<User> {
+    return axios.post(loginApi, {email, password})
 }
 
-export function registerNewUser(username: string, password: string,
-                         lastname: string, firstname: string): Promise<boolean> {
-    return axios.post(registerApi, {username, password})
+export function registerNewUser(email: string, password: string,
+                                lastname: string, firstname: string): Promise<boolean> {
+    return axios.post(registerApi, {
+        user: {
+            email, lastname, firstname
+        },
+        password
+    })
 }
